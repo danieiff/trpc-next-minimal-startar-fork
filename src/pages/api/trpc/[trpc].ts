a4e6e930cdad6c3896f334f064994cb2235a7b8b
danieiff <sugielff@gmail.com>
2023-04-15 13:40:28 +0900

    Add user logic to trpc procedures

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,15 +2,48 @@ import { initTRPC } from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { z } from "zod";
 
+export interface User {
+  id: string;
+  name: string;
+}
+
+const userList: User[] = [
+  {
+    id: "1",
+    name: "KATT",
+  },
+];
+
 const t = initTRPC.create();
+const publicProcedure = t.procedure;
 
 export const appRouter = t.router({
-  hello: t.procedure
+  userById: publicProcedure
+    .input((val: unknown) => {
+      if (typeof val === "string") return val;
+      throw new Error(`Invalid input: ${typeof val}`);
+    })
+    .query((req) => {
+      const { input } = req;
+      const user = userList.find((it) => it.id === input);
+      return user;
+    }),
+  userCreate: publicProcedure
+    .input(z.object({ id: z.string(), name: z.string() }))
+    .mutation((req) => {
+      const user: User = {
+        id: req.input.id,
+        name: req.input.name,
+      };
+      userList.push(user);
+      return user;
+    }),
+  hello: publicProcedure
     // Try changing the input type and see the typeerrors on the client
     .input(z.object({ text: z.string().nullish() }).nullish())
     .query(({ input }) => {
       return {
-        greeting: `hello ${input?.text ?? "world"}`,
+        greeting_text: `hello ${input?.text ?? "world"}`,
       };
     }),
 });
